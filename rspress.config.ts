b73fcd3903c737bd618c54ff49be1f19c6e54ae9
王昆宇 <wangkunyu@social-touch.com>
2025-03-12 17:30:36 +0800

    feat: 基础图形文档编写完成

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,21 +1,12 @@
 import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
-
+import { pluginPreview } from '@rspress/plugin-preview';
 export default defineConfig({
-  // root: path.join(__dirname, 'docs'),
-  // title: 'graphify-chart',
-  // icon: '/rspress-icon.png',
-  // logo: {
-  //   light: '/rspress-light-logo.png',
-  //   dark: '/rspress-dark-logo.png',
-  // },
-  // themeConfig: {
-  //   socialLinks: [
-  //     {
-  //       icon: 'github',
-  //       mode: 'link',
-  //       content: 'https://github.com/web-infra-dev/rspress',
-  //     },
-  //   ],
-  // },
+  plugins: [pluginPreview({
+    previewMode:'internal',
+    iframeOptions:{
+      devPort: 3030,
+      position:'fixed'
+    }
+  })],
 });
